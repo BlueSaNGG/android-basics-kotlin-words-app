817559cad8df48b57e41b30b84b846b2cb484ff9
qjin1 <qjin1@paypal.com>
2023-03-27 14:38:48 -0500

    using nav graph
    using navhostcontroller
    using navgraph
    passing arg between fragment using navgraph (recieve on create from arguments)
    update appbar using navgraph(fragment label)

--- app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -19,7 +19,15 @@ class WordListFragment : Fragment() {
 
     private var _binding: FragmentWordListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var letterId: String
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            letterId = it.getString(LETTER).toString()
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,7 +38,6 @@ class WordListFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val letterId = activity?.intent?.extras?.getString(LETTER).toString()
         val recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(this.requireContext())
         recyclerView.adapter = WordAdapter(letterId, this.requireContext())
